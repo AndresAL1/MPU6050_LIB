2e699d0b6afa6b68386298040379d88e3347680c
Andres AL <aguinagaanlo@gmail.com>
2024-01-04 02:07:00 -0600

    Implemented accelerometer and gyroscope offset structures for calibration handling.

--- inc/MPU6050_LIB.h
+++ inc/MPU6050_LIB.h
@@ -86,6 +86,21 @@ typedef struct {
 	float convertedRotaZ;
 } MPU6050_Rotations;
 
+// MPU6050 Accelerometer Offset structure
+typedef struct {
+    int16_t xOffset;
+    int16_t yOffset;
+    int16_t zOffset;
+} MPU6050_AccelOffsets;
+
+// MPU6050 Gyroscope Offset structure
+typedef struct {
+    int16_t xOffset;
+    int16_t yOffset;
+    int16_t zOffset;
+} MPU6050_GyroOffsets;
+
+
 // Errors enumeration
 typedef enum {
     INIT_OK = 0,
